89ce3555fbecdce10ad0c1e35da922f3143adc2d
Rachel <rachel17.22@gmail.com>
2020-06-05 13:28:08 -0600

    dropdown selection for types added, needs to be connected to firestore collection types

--- src/components/pages/PageCreateCards/PageCreateCards.jsx
+++ src/components/pages/PageCreateCards/PageCreateCards.jsx
@@ -5,12 +5,23 @@ import firebase from '../../../firebase/firebase';
 
 function PageCreateCards() {
   const [cardName, setCardName] = React.useState('');
+  const [sideOne, setSideOne] = React.useState('');
+  const [sideTwo, setSideTwo] = React.useState('');
+  const [sideThree, setSideThree] = React.useState('');
+  const [sideFour, setSideFour] = React.useState('');
+  const [typeValue, setTypeValue] = React.useState('');
+
   const user = useSelector((state) => state.user);
 
   async function handleCreateCard() {
     const newCard = {
       cardTitle: cardName,
+      type: typeValue,
       createdBy: user.uid,
+      side1: sideOne,
+      side2: sideTwo,
+      side3: sideThree,
+      side4: sideFour,
     };
 
     await firebase.db.collection('cards').add(newCard);
@@ -25,7 +36,6 @@ function PageCreateCards() {
         <form onSubmit={async (evt) => {
           evt.preventDefault();
           setCardName('');
-
           await handleCreateCard();
         }}
         >
@@ -40,6 +50,69 @@ function PageCreateCards() {
               value={cardName}
               type="text"
               placeholder="card name"
+              autoComplete="off"
+            />
+          </label>
+          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+          <label>
+            Type: &nbsp;
+            <select
+              data-test="type-value"
+              onChange={(event) => {
+                setTypeValue(event.target.value);
+              }}
+              value={typeValue}
+            >
+              <option value="">--Please choose a type--</option>
+              <option value="character">Character</option>
+              <option value="conflict">Conflict</option>
+              <option value="circumstance">Circumstance</option>
+            </select>
+          </label>
+          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+          <label>
+            Sides: &nbsp;
+            <input
+              data-test="side-input-one"
+              onChange={(event) => {
+                setSideOne(event.target.value);
+              }}
+              value={sideOne}
+              type="text"
+              placeholder="side one"
+              autoComplete="off"
+            />
+
+            <input
+              data-test="side-input-two"
+              onChange={(event) => {
+                setSideTwo(event.target.value);
+              }}
+              value={sideTwo}
+              type="text"
+              placeholder="side two"
+              autoComplete="off"
+            />
+
+            <input
+              data-test="side-input-three"
+              onChange={(event) => {
+                setSideThree(event.target.value);
+              }}
+              value={sideThree}
+              type="text"
+              placeholder="side three"
+              autoComplete="off"
+            />
+
+            <input
+              data-test="side-input-four"
+              onChange={(event) => {
+                setSideFour(event.target.value);
+              }}
+              value={sideFour}
+              type="text"
+              placeholder="side four"
               autoComplete="off"
             />
           </label>
--- src/components/pages/PageCreateCards/PageCreateCards.test.jsx
+++ src/components/pages/PageCreateCards/PageCreateCards.test.jsx
@@ -9,9 +9,9 @@ import { findByTestAttr } from '../../../tests/testUtils';
 import PageCreateCards from './PageCreateCards';
 
 describe('Create cards page', () => {
-  const setup = (user) => {
+  const setup = (user, type) => {
     const mockStore = configureMockStore([]);
-    const store = mockStore({ user });
+    const store = mockStore({ user, type });
 
     const wrapper = mount(
       <Provider store={store}>
@@ -29,16 +29,10 @@ describe('Create cards page', () => {
     expect(component.length).toBe(1);
   });
 
-  describe('state controlled input fields', () => {
-    const user = { uid: '1234' };
-    const { wrapper } = setup(user);
-    const mockSetCurrentInput = jest.fn();
-
-    beforeEach(() => {
-      mockSetCurrentInput.mockClear();
-    });
-
+  describe('Create card form', () => {
     it('should send the filled in inputs to firebase', async () => {
+      const user = { uid: '1234' };
+      const type = { character: 'typeValue' };
       // mock firestore data model and spyOn adding to the cards collection
       const spyAdd = jest.fn();
       spyAdd.mockReturnValue(() => new Promise((resolve) => resolve()));
@@ -47,25 +41,68 @@ describe('Create cards page', () => {
       spyFirebaseDbCollection.mockReturnValue({
         add: spyAdd,
       });
-
       const cardTitle = 'Sample title';
       const userId = '1234';
+      const character = 'typeValue';
+      const side1 = 'first side text';
+      const side2 = 'second side text';
+      const side3 = 'third side text';
+      const side4 = 'fourth side text';
 
-      // Inputs with enzyme
+      // data model sent to firestore
+      const newCard = {
+        cardTitle,
+        type: character,
+        createdBy: userId,
+        side1,
+        side2,
+        side3,
+        side4,
+      };
+
+      // INPUTS WITH ENZYME
+      const { wrapper } = setup(user, type);
+
+      // Card Title Input
       const nameInput = findByTestAttr(wrapper, 'card-name');
 
       const mockTitleInput = { target: { value: 'Sample title' } };
       nameInput.simulate('change', mockTitleInput);
 
-      // Click submit
+      // Card Type Selection
+      const typeSelection = findByTestAttr(wrapper, 'type-value');
+
+      const mockTypeSelection = { target: { value: 'typeValue' } };
+      typeSelection.simulate('change', mockTypeSelection);
+
+      // Side input (1/4)
+      const sideInputOne = findByTestAttr(wrapper, 'side-input-one');
+
+      const mockSideInputOne = { target: { value: 'first side text' } };
+      sideInputOne.simulate('change', mockSideInputOne);
+
+      // Side input (2/4)
+      const sideInputTwo = findByTestAttr(wrapper, 'side-input-two');
+
+      const mockSideInputTwo = { target: { value: 'second side text' } };
+      sideInputTwo.simulate('change', mockSideInputTwo);
+
+      // Side input (3/4)
+      const sideInputThree = findByTestAttr(wrapper, 'side-input-three');
+
+      const mockSideInputThree = { target: { value: 'third side text' } };
+      sideInputThree.simulate('change', mockSideInputThree);
+
+      // Side input (4/4)
+      const sideInputFour = findByTestAttr(wrapper, 'side-input-four');
+
+      const mockSideInputFour = { target: { value: 'fourth side text' } };
+      sideInputFour.simulate('change', mockSideInputFour);
+
+      // CLICK SUBMIT
       const submitButton = findByTestAttr(wrapper, 'save-card');
       await submitButton.simulate('submit');
 
-      // data model sent to firestore
-      const newCard = {
-        cardTitle,
-        createdBy: userId,
-      };
       expect(spyAdd).toHaveBeenCalledWith(newCard);
     });
   });
