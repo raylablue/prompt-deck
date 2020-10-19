cf954c4c814ab46c6a76224da73b5bbd066c638f
Rachel <rachel17.22@gmail.com>
2020-10-19 14:01:52 -0600

    initial display of test card for prompt and render tests

--- src/components/pages/PageHome/PageHome.jsx
+++ src/components/pages/PageHome/PageHome.jsx
@@ -1,11 +1,45 @@
 import React from 'react';
 import TemplateDefault from '../../Templates/TemplateDefault';
+import CardsDisplay from '../../molecules/CardsDisplay/CardsDisplay';
 
 function PageHome() {
+  const cardDefault = {
+    cardTitle: 'Card',
+    type: 'lol',
+    side1: 'this',
+    side2: 'is',
+    side3: 'a',
+    side4: 'test',
+  };
+  // firebase collections helper call to
+    // firebase.db.collection('decks').where("visibility", "==", "featured").get
+  // get the card data off of the deck by type
+  // randomly get one card from each category and set card or typeCard value to that
+    // this random getting should be a re-usable button call,
+    // rather than re-calling firebase every time.
+  // pass that state to CardsDisplay to display the new card
   return (
     <TemplateDefault data-test="p-home">
       <h1>Home</h1>
-      <p>home page things will go here</p>
+      <h2>Prompt</h2>
+
+      <div data-test="p-home__prompt">
+        <CardsDisplay
+          data-test="p-home__character-card"
+          card={cardDefault}
+        />
+
+        <CardsDisplay
+          data-test="p-home__circumstance-card"
+          card={cardDefault}
+        />
+
+        <CardsDisplay
+          data-test="p-home__conflict-card"
+          card={cardDefault}
+        />
+      </div>
+
     </TemplateDefault>
   );
 }
--- src/components/pages/PageHome/PageHome.test.jsx
+++ src/components/pages/PageHome/PageHome.test.jsx
@@ -15,4 +15,29 @@ describe('Home page', () => {
     const component = findByTestAttr(wrapper, 'p-home');
     expect(component.length).toBe(1);
   });
+  describe('Prompt', () => {
+    it('should render the prompt without error', () => {
+      const { wrapper } = setup();
+      const prompt = findByTestAttr(wrapper, 'p-home__prompt');
+      expect(prompt.length).toBe(1);
+    });
+
+    it('should render the character card without error', () => {
+      const { wrapper } = setup();
+      const characterCard = findByTestAttr(wrapper, 'p-home__character-card');
+      expect(characterCard.length).toBe(1);
+    });
+
+    it('should render the circumstance card without error', () => {
+      const { wrapper } = setup();
+      const circumstanceCard = findByTestAttr(wrapper, 'p-home__circumstance-card');
+      expect(circumstanceCard.length).toBe(1);
+    });
+
+    it('should render the conflict card without error', () => {
+      const { wrapper } = setup();
+      const conflictCard = findByTestAttr(wrapper, 'p-home__conflict-card');
+      expect(conflictCard.length).toBe(1);
+    });
+  });
 });
